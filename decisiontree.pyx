dfbcb0a145c5457daf3dc6084b5d0fd908e509a9
galustian <leonard@galustian.com>
2018-01-13 13:39:07 +0100

    add recurse_tree function

--- decisiontree.pyx
+++ decisiontree.pyx
@@ -4,6 +4,11 @@ cimport cython
 from numpy cimport ndarray, float64_t, int_t
 from cython.parallel import prange
 
+cdef double[:, ::1] bootstrap_data(double[:, ::1] data):
+    return data[np.random.choice(data.shape[0], data.shape[0])]
+
+cdef bool is_pure(double[:] Y):
+    return np.unique(Y).size == 1
 
 #@cython.boundscheck(False)
 @cython.wraparound(False)
@@ -11,9 +16,9 @@ from cython.parallel import prange
 cpdef double gini_index(double[:, ::1] left, double[:, ::1] right):
     cdef:
         double purity = 0.0
+        double class_ratio
         double[:, ::1] split
         int[:] class_counts, unique_classes
-        double class_ratio
         int total_classes, bi, i
 
     for bi in range(2):
@@ -57,16 +62,48 @@ cpdef dict get_best_split(double[:, ::1] data):
     return {'left': b_left, 'right': b_right, 'split_point': b_split_point, 'predictor': b_predictor, 'gini': b_gini}
 
 
-'''
-# 
-cdef dict recurse_tree(dict node)
-'''
+cdef dict recurse_tree(dict node, int depth, int max_depth, int max_x):
+    cdef:
+        dict left_node, right_node
+        int most_common_class
+    
+    # check branch depth
+    if depth >= max_depth:
+        most_common_class = np.bincount(node.left[:, -1]).argmax()
+        node['left_node'] = {'end_node': True, 'y_hat': most_common_class}
 
-cdef double[:, ::1] bootstrap_data(double[:, ::1] data):
-    return data[np.random.choice(data.shape[0], data.shape[0])]
+        most_common_class = np.bincount(node.right[:, -1]).argmax()
+        node['right_node'] = {'end_node': True, 'y_hat': most_common_class}
+        return
+    
+    # check max_x & convergence
+    if node.left.shape[0] <= max_x or is_pure(node.left[:, -1]):
+        most_common_class = np.bincount(node.left[:, -1]).argmax()
+        node['left_node'] = {'end_node': True, 'y_hat': most_common_class}
+        return
+    else:
+        left_node = get_best_split(node.left)
+        del node['left']
+        node['left_node'] = left_node
+        recurse_tree(node['left_node'], depth+1, max_depth, max_x)
+
+
+    if node.right.shape[0] <= max_x or is_pure(node.right[:, -1]):
+        most_common_class = np.bincount(node.right[:, -1]).argmax()
+        node['right_node'] = {'end_node': True, 'y_hat': most_common_class}
+        return
+    else:
+        right_node = get_best_split(node.right)
+        del node['right']
+        node['right_node'] = right_node
+        recurse_tree(node['right_node'], depth+1, max_depth, max_x)
+
+    # entire decision-tree
+    return node
+
 
 # labels have to be last column
-cdef create_tree(double[:, ::1] data, int bootstrap_size, int max_x)
+cdef create_tree(double[:, ::1] data, int bootstrap_size, int max_depth, int max_x)
     cdef:
         list bagged_trees = []
         dict tree
@@ -74,5 +111,5 @@ cdef create_tree(double[:, ::1] data, int bootstrap_size, int max_x)
 
     for i in range(bootstrap_size):        
         cdef dict root_node = get_best_split(bootstrap_data(data))
-        tree = recurse_tree(root_node)
+        tree = recurse_tree(root_node, 1, max_depth, max_x)
         bagged_trees.append(tree)
